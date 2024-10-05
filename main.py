d9c2e99e837a6db856138437c9eb207844b6f538
Lisa <vasi.markova8@yandex.ru>
2024-10-05 22:08:07 +0300

    Task #1

--- main.py
+++ main.py
@@ -19,5 +19,32 @@ def read_cookbook(file_name):
             file.readline()
     return cook_book
 
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+
+    for dish in dishes:
+        if dish in cook_book:
+            for ingredient in cook_book[dish]:
+                ingredient_name = ingredient['ingredient_name']
+                quantity = ingredient['quantity'] * person_count
+                measure = ingredient['measure']
+
+                if ingredient_name in shop_list:
+
+                    shop_list[ingredient_name]['quantity'] += quantity
+                else:
+
+                    shop_list[ingredient_name] = {'measure': measure, 'quantity': quantity}
+        else:
+            print(f"Блюдо '{dish}' отсутствует в книге рецептов.")
+
+    return shop_list
+
+
 cook_book = read_cookbook('recipes.txt')
-print(cook_book)
+
+shop_list = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+
+print(shop_list)
